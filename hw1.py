87963c9beb60ce0f2472b5f1b3aeba71c0633ff3
Alexander Novak <sasha_7tdd7@mail.ru>
2023-09-16 22:15:26 +0300

    Ready for batch support

--- hw1.py
+++ hw1.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import MinMaxScaler
 
 
 class Activation:
@@ -46,10 +52,14 @@ class Layer:
     ):
         self.learning_rate = learning_rate
         self.biases = np.random.rand(size).reshape(-1, 1) / 10
+        self.dl_db = np.zeros_like(self.biases)
         if prev_size is not None and prev_size != 0:
             self.weights = np.random.rand(size, prev_size) / 10
+            self.dl_dw = np.zeros_like(self.weights)
 
         self._activation = activation
+
+        self._backward_count = 0
 
     def forward(self, input: np.ndarray) -> np.ndarray:
         if input.ndim == 0:
@@ -58,26 +68,30 @@ class Layer:
             self.neurons_input = self.weights @ input + self.biases
         return self._activation.forward(self.neurons_input)
 
-    def backward_with_update(
-        self, error: np.ndarray, prev_out: np.ndarray
-    ) -> np.ndarray:
+    def backward(self, error: np.ndarray, prev_out: np.ndarray) -> np.ndarray:
+        self._backward_count += 1
+
         dl_dout = error.reshape(-1, 1)
-        limit = 1
-        dl_dout = np.where(dl_dout < limit, dl_dout, limit)
-        dl_dout = np.where(dl_dout > -limit, dl_dout, -limit)
+        # limit = 1
+        # dl_dout = np.where(dl_dout < limit, dl_dout, limit)
+        # dl_dout = np.where(dl_dout > -limit, dl_dout, -limit)
 
         neurons_backward = dl_dout * self._activation.backward(self.neurons_input)
         neurons_backward = neurons_backward.reshape(-1)
 
-        dl_dw = np.outer(neurons_backward, prev_out)
-        dl_db = neurons_backward
+        self.dl_dw += np.outer(neurons_backward, prev_out)
+        self.dl_db += neurons_backward.reshape(-1, 1)
 
         dl_dout = self.weights.T @ neurons_backward
-
-        self.weights -= self.learning_rate * dl_dw
-        self.biases -= self.learning_rate * dl_db.reshape(-1, 1)
-
         return dl_dout
+
+    def update_weights(self) -> None:
+        self.weights -= self.learning_rate * self.dl_dw / self._backward_count
+        self.biases -= self.learning_rate * self.dl_db / self._backward_count
+
+        self.dl_dw = np.zeros_like(self.dl_dw)
+        self.dl_db = np.zeros_like(self.dl_db)
+        self._backward_count = 0
 
 
 class MLPRegressor:
@@ -127,18 +141,24 @@ class MLPRegressor:
             self._init_knowing_sizes(x, y)
 
         self._x_norm_coef = x.mean()
-        x /= self._x_norm_coef
+        x = x / self._x_norm_coef
 
         self._y_norm_coef = y.mean()
-        y /= self._y_norm_coef
+        y = y / self._y_norm_coef
 
         losses = []
+
         for epoch in range(self._max_iter):
+            i = 0
             for inp, actual in zip(x, y):
                 predicted = self._forward(inp, True)
                 loss = self._loss(actual, predicted)
-                self._update_weights(actual, predicted)
-                losses.append(loss)
+                self._backward(actual, predicted)
+                self._update_weights()
+                i += 1
+                if i % int(x.shape[0] / 10) == 1:
+                    losses.append(loss)
+
         return losses
 
     def predict(self, x: np.ndarray):
@@ -154,7 +174,7 @@ class MLPRegressor:
             ), """Input size differs from training! 
                     current = {x.shape[-1]}, during training = {self._input_size}"""
 
-        x /= self._x_norm_coef
+        x = x / self._x_norm_coef
 
         result = []
         if x.ndim == 2 or (x.ndim == 1 and self._input_size == 1):
@@ -165,13 +185,13 @@ class MLPRegressor:
 
         return np.array(result) * self._y_norm_coef
 
-    def _forward(self, single_input: np.ndarray, train: bool = False) -> np.ndarray:
-        assert single_input.ndim == 1 or single_input.ndim == 0
+    def _forward(self, inp: np.ndarray, train: bool = False) -> np.ndarray:
+        assert inp.ndim == 1 or inp.ndim == 0
 
         if train:
-            self._neurons_outputs = [single_input]
+            self._neurons_outputs = [inp]
 
-        last_layer_out = single_input.reshape(-1, 1)
+        last_layer_out = inp.reshape(-1, 1)
 
         for layer in self._layers:
             last_layer_out = layer.forward(last_layer_out)
@@ -180,13 +200,15 @@ class MLPRegressor:
 
         return last_layer_out.reshape(-1)
 
-    def _update_weights(self, actual: np.ndarray, predicted: np.ndarray) -> None:
+    def _backward(self, actual: np.ndarray, predicted: np.ndarray) -> None:
         dl_dout = self._loss_derivative(actual, predicted)
 
         for i in range(len(self._layers) - 1, -1, -1):
-            dl_dout = self._layers[i].backward_with_update(
-                dl_dout, self._neurons_outputs[i]
-            )
+            dl_dout = self._layers[i].backward(dl_dout, self._neurons_outputs[i])
+
+    def _update_weights(self) -> None:
+        for i in range(len(self._layers) - 1, -1, -1):
+            self._layers[i].update_weights()
 
     def _init_knowing_sizes(self, x: np.ndarray, y: np.ndarray) -> None:
         if x.ndim == 1:
@@ -197,6 +219,7 @@ class MLPRegressor:
         self._layers[0].weights = (
             np.random.rand(first_layer_size, self._input_size) / 10
         )
+        self._layers[0].dl_dw = np.zeros_like(self._layers[0].weights)
 
         if y.ndim == 1:
             self._output_size = 1
@@ -208,27 +231,59 @@ class MLPRegressor:
         )
 
 
+# dataset
+diamonds_df = pd.read_csv("diamonds.csv")
+features = ["carat", "cut", "color", "clarity", "depth", "table", "x", "y", "z"]
+target = ["price"]
+cut_transform = {"Fair": 0, "Good": 1, "Very Good": 2, "Premium": 3, "Ideal": 4}
+clarity_transform = {
+    "I1": 0,
+    "SI2": 1,
+    "SI1": 2,
+    "VS2": 3,
+    "VS1": 4,
+    "VVS2": 5,
+    "VVS1": 6,
+    "IF": 7,
+}
+color_transorm = {"D": 0, "E": 1, "F": 2, "G": 3, "H": 4, "I": 5, "J": 6}
+diamonds_df["cut"] = diamonds_df["cut"].apply(lambda x: cut_transform.get(x))
+diamonds_df["color"] = diamonds_df["color"].apply(lambda x: color_transorm.get(x))
+diamonds_df["clarity"] = diamonds_df["clarity"].apply(
+    lambda x: clarity_transform.get(x)
+)
+X = diamonds_df[features].copy().values
+y = diamonds_df[target].values
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, random_state=47, test_size=0.3
+)
+
+
 # Generate a dataset
 x = np.linspace((0, 0), (1000, 1000), 100)
-y = 2 * x + 1
+y = 2 * x * x + 1
 
 # Create an MLPRegressor object
 regressor = MLPRegressor(
-    hidden_layer_sizes=(2,),
+    hidden_layer_sizes=(10,),
     learning_rate=0.001,
-    max_iter=100,
-    activation=LeakedReLu(1),
+    max_iter=200,
+    activation=LeakedReLu(0.1),
 )
 
 # Train the regressor
-losses = regressor.train(x, y)
+# losses = regressor.train(X_train, y_train)
+# plt.plot(np.array(losses))
 
+losses = regressor.train(x, y)
 plt.plot(np.array(losses)[:, 0])
 plt.plot(np.array(losses)[:, 1])
 plt.show()
 
 # Predict the values of y for the given values of x
-predicted_y = regressor.predict(x)
+predicted_y = regressor.predict(X_test)
 
 # Plot the results
 plt.figure()
